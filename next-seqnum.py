bcfbcc52726e492739ee44e6b130799f13dae609
Brian Warner <warner@lothar.com>
2014-04-04 11:25:55 -0700

    next-seqnum: scan multiple directories
    Call this with e.g. "next-seqnum.py drafts published".

--- next-seqnum.py
+++ next-seqnum.py
@@ -1,24 +1,28 @@
 import os, sys, re
 
-next_seqnum = 0
-for root, dirs, files in os.walk(sys.argv[1]):
-    for fn in files:
-        afn = os.path.join(root, fn)
-        if fn.endswith(".rst"):
-            for line in open(afn):
-                mo = re.search(r'^:slug:\s+(\d+)-', line.lower())
-                if mo:
-                    next_seqnum = max(next_seqnum, int(mo.group(1)))
-                    break
-            else:
-                raise RuntimeError("no :slug: in %s" % afn)
-        if fn.endswith(".md"):
-            for line in open(afn):
-                mo = re.search(r'^slug:\s+(\d+)-', line.lower())
-                if mo:
-                    next_seqnum = max(next_seqnum, int(mo.group(1)))
-                    break
-            else:
-                raise RuntimeError("no slug: in %s" % afn)
-next_seqnum += 1
-print "%02d" % next_seqnum
+def find_seqnums(rootdir):
+    for root, dirs, files in os.walk(rootdir):
+        for fn in files:
+            afn = os.path.join(root, fn)
+            if fn.endswith(".rst"):
+                for line in open(afn):
+                    mo = re.search(r'^:slug:\s+(\d+)-', line.lower())
+                    if mo:
+                        yield int(mo.group(1))
+                        break
+                else:
+                    raise RuntimeError("no :slug: in %s" % afn)
+            if fn.endswith(".md"):
+                for line in open(afn):
+                    mo = re.search(r'^slug:\s+(\d+)-', line.lower())
+                    if mo:
+                        yield int(mo.group(1))
+                        break
+                else:
+                    raise RuntimeError("no slug: in %s" % afn)
+
+seqnums = set()
+for dirname in sys.argv[1:]:
+    seqnums.update(find_seqnums(dirname))
+max_seqnum = max(seqnums)
+print "%02d" % (max_seqnum+1)
